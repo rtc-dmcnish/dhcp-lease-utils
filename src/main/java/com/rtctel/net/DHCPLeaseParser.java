8217f7dfecb7f4e196a3dc6ff86986dd213571b5
dmcnish <dmcnish@rtctel.com>
2016-01-27 09:08:39 -0500

    Initial commit

---
+++ src/main/java/com/rtctel/net/DHCPLeaseParser.java
@@ -0,0 +1,49 @@
+package com.rtctel.net;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class DHCPLeaseParser {
+	
+	private Scanner scanner = null; 
+	private List<String> activeLeases; 
+	
+	public DHCPLeaseParser(String leaseFile) {
+		try {
+			this.scanner = new Scanner(new File(leaseFile));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}   
+	}
+	
+	public List<String> allActiveLeases() {
+		activeLeases = new ArrayList<String>(); 
+		String lease = null; 
+		while (scanner.hasNextLine()) {
+			if (scanner.hasNext("lease")) {
+				scanner.next(); 
+				lease = scanner.next(); 
+				scanner.nextLine(); 
+			} else if (scanner.hasNext("binding")) {
+				scanner.next();
+				scanner.next();
+				String bState = scanner.next(); 
+				if (bState.equals("active;") && !lease.isEmpty()) {
+					activeLeases.add(lease);
+				}
+				scanner.nextLine(); 
+			}
+			scanner.nextLine(); 
+		}
+		
+		return activeLeases; 
+
+	}
+	
+	
+
+}
---
+++ src/test/java/com/rtctel/net/DHCPLeaseParserTest.java
@@ -0,0 +1,11 @@
+package com.rtctel.net;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+
+public class DHCPLeaseParserTest extends TestCase {
+	
+	public void testShutUpMavenIWillWriteUnitTestsLater() {
+		Assert.assertTrue(true);
+	}
+}
